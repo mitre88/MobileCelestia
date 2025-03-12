975224de12335df544d27aafb58a72d802a59069
Levin Li <lilinfeng303@outlook.com>
2025-03-11 18:07:26 -0700

    Use ANGLE on all platforms

--- MobileCelestia/Celestia/CelestiaDisplayController.swift
+++ MobileCelestia/Celestia/CelestiaDisplayController.swift
@@ -7,7 +7,7 @@
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
 
-import AsyncGL
+import AsyncGLANGLE
 import CelestiaCore
 import CelestiaFoundation
 import CelestiaUI
@@ -68,7 +68,7 @@ class CelestiaDisplayController: AsyncGLViewController {
         featureFlags: FeatureFlags
     ) {
 #if targetEnvironment(macCatalyst)
-        let api = AsyncGLAPI.openGLLegacy
+        let api = AsyncGLAPI.openGLES2
 #else
         let api = featureFlags.iosGLESV3 ? AsyncGLAPI.openGLES3 : AsyncGLAPI.openGLES2
 #endif
